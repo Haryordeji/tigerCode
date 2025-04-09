f2895ecdda2e2ddaa1bf2b6a240181db6a4ee396
Ayo Ogunsanya <aaolamiji@gmail.com>
2025-04-09 14:52:14 -0400

    test

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
-
+  console.log('VITE_API_URL:', env.VITE_API_URL)
   return {
     plugins: [react()],
     server: {
@@ -17,4 +17,6 @@ export default defineConfig(({ mode }) => {
       },
     },
   }
+
+
 })
